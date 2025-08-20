254f891b674476b78ca231232b7225268908aa56
Elissandro Vieira | casa <teofiloeli@gmail.com>
2025-08-19 22:39:51 -0300

    Start philosophers dining exercise

---
+++ philo/src/philo/Main.java
@@ -0,0 +1,7 @@
+package com.philo;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ philo/src/philo/model/Philosopher.java
@@ -0,0 +1,8 @@
+package philo.com.model
+
+public class Philosopher extends Thread {
+	
+	@Override
+	public void run() {
+	}
+}
